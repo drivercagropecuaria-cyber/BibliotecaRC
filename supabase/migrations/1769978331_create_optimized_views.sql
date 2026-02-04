1548e958388ea676549bb8ff665cb4e46c0928d6
drivercagropecuaria <drivercagropecuaria@gmail.com>
2026-02-03 21:26:13 -0300

    Fix optimized views joins for taxonomy_categories

--- supabase/migrations/1769978331_create_optimized_views.sql
+++ supabase/migrations/1769978331_create_optimized_views.sql
@@ -38,19 +38,19 @@ SELECT
   ci.status_id,
   ci.capitulo_id,
   
-  -- Nomes das tabelas de lookup
-  af.nome as area_fazenda_nome,
-  p.nome as ponto_nome,
-  tp.nome as tipo_projeto_nome,
-  np.nucleo as nucleo_pecuaria_nome,
-  na.nucleo as nucleo_agro_nome,
-  oi.nucleo as operacao_nome,
-  mv.nucleo as marca_nome,
-  ep.nome as evento_nome,
-  fh.nome as funcao_historica_nome,
-  tprinc.nome as tema_principal_nome,
-  sm.nome as status_nome,
-  cf.nome as capitulo_nome,
+  -- Nomes das taxonomias
+  af.name as area_fazenda_nome,
+  p.name as ponto_nome,
+  tp.name as tipo_projeto_nome,
+  np.name as nucleo_pecuaria_nome,
+  na.name as nucleo_agro_nome,
+  op.name as operacao_nome,
+  mv.name as marca_nome,
+  ep.name as evento_nome,
+  fh.name as funcao_historica_nome,
+  tprinc.name as tema_principal_nome,
+  sm.name as status_nome,
+  cf.name as capitulo_nome,
   
   -- Dados de mídia
   ma.filename as arquivo_nome,
@@ -68,18 +68,18 @@ SELECT
   ci.deleted_at
   
 FROM catalogo_itens ci
-LEFT JOIN areas_fazendas af ON ci.area_fazenda_id = af.id
-LEFT JOIN pontos p ON ci.ponto_id = p.id
-LEFT JOIN tipos_projeto tp ON ci.tipo_projeto_id = tp.id
-LEFT JOIN nucleos_pecuaria np ON ci.nucleo_pecuaria_id = np.id
-LEFT JOIN nucleos_agro na ON ci.nucleo_agro_id = na.id
-LEFT JOIN operacoes_internas oi ON ci.operacao_id = oi.id
-LEFT JOIN marca_valorizacao mv ON ci.marca_id = mv.id
-LEFT JOIN eventos_principais ep ON ci.evento_id = ep.id
-LEFT JOIN funcoes_historicas fh ON ci.funcao_historica_id = fh.id
-LEFT JOIN temas_principais tprinc ON ci.tema_principal_id = tprinc.id
-LEFT JOIN status_material sm ON ci.status_id = sm.id
-LEFT JOIN capitulos_filme cf ON ci.capitulo_id = cf.id
+LEFT JOIN taxonomy_categories af ON ci.area_fazenda_id = af.id
+LEFT JOIN taxonomy_categories p ON ci.ponto_id = p.id
+LEFT JOIN taxonomy_categories tp ON ci.tipo_projeto_id = tp.id
+LEFT JOIN taxonomy_categories np ON ci.nucleo_pecuaria_id = np.id
+LEFT JOIN taxonomy_categories na ON ci.nucleo_agro_id = na.id
+LEFT JOIN taxonomy_categories op ON ci.operacao_id = op.id
+LEFT JOIN taxonomy_categories mv ON ci.marca_id = mv.id
+LEFT JOIN taxonomy_categories ep ON ci.evento_id = ep.id
+LEFT JOIN taxonomy_categories fh ON ci.funcao_historica_id = fh.id
+LEFT JOIN taxonomy_categories tprinc ON ci.tema_principal_id = tprinc.id
+LEFT JOIN taxonomy_categories sm ON ci.status_id = sm.id
+LEFT JOIN taxonomy_categories cf ON ci.capitulo_id = cf.id
 LEFT JOIN media_assets ma ON ci.media_id = ma.id
 WHERE ci.deleted_at IS NULL;
 

4f603d046338e194ca19f8b4d333f45f7d598b98
drivercagropecuaria <drivercagropecuaria@gmail.com>
2026-02-03 21:03:19 -0300

    Adicionar overlay de erro global

--- project_analysis/acervo-rc/src/main.tsx
+++ project_analysis/acervo-rc/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'sonner'
 import { ErrorBoundary } from './components/ErrorBoundary.tsx'
+import { GlobalErrorOverlay } from './components/GlobalErrorOverlay.tsx'
 import './index.css'
 import App from './App.tsx'
 
@@ -25,6 +26,7 @@ createRoot(document.getElementById('root')!).render(
       <ErrorBoundary>
         <App />
         <Toaster richColors position="top-right" />
+        <GlobalErrorOverlay />
       </ErrorBoundary>
     </QueryClientProvider>
   </StrictMode>,
